58f710edc4d2cb46716ef810cc125bcb231f811d
Mana-0 <mohamdgamer11@gmail.com>
2026-07-23 12:31:59 +0300

    done

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-width = int(input())
-height = int(input())
+a = int(input())
+b = int(input())
 
-area = width * height
-perimeter = 2 * (width + height)
-
-print(area)
-print(perimeter)
+print(a + b)
