db8c380312ef92613ae4a68574bf0699a2a0c51b
Mana-0 <mohamdgamer11@gmail.com>
2026-07-23 13:29:06 +0300

    done

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-num = int(input())
+score = int(input())
 
-print('zero' if num == 0 else 'positive' if num > 0 else 'negative')
-
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")      
 
